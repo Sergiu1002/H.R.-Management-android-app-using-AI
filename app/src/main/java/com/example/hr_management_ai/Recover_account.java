9c6d4c8e34dda75c680ae4eff06cb06c383e1ec7
Sergiu1002 <sergiutudosi@yahoo.com>
2023-04-04 10:30:43 +0300

    Adaugat pagina de recover account

--- app/src/main/java/com/example/hr_management_ai/Recover_account.java
+++ app/src/main/java/com/example/hr_management_ai/Recover_account.java
@@ -7,19 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Recover_account extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recover_account);
 
-        Button RecoverAccount = (Button) findViewById(R.id.buttonRecoverAccount);
+
+        Button RecoverAccount = (Button) findViewById(R.id.buttonRecoverAccount2);
 
         RecoverAccount.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 // Create an intent to start the second activity
-                Intent intent = new Intent(MainActivity.this, Recover_account.class);
+                Intent intent = new Intent(Recover_account.this, MainActivity.class);
 
                 // Add any data you want to save as extras to the intent
 //                EditText editText = findViewById(R.id.editText);
