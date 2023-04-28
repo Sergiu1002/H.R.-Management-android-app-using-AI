be63f873872971908a3b774b38bcfe178c38ea73
Sergiu1002 <sergiutudosi@yahoo.com>
2023-04-28 13:36:41 +0300

    tried header and dropdown, still work in progress

--- app/src/main/java/com/example/hr_management_ai/feed_page.java
+++ app/src/main/java/com/example/hr_management_ai/feed_page.java
@@ -1,13 +1,37 @@
 package com.example.hr_management_ai;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class feed_page extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +39,97 @@ public class feed_page extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_feed_page);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar_view);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_open, R.string.menu_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        ImageButton LogoButton = findViewById(R.id.toolbar_logo_button);
+        EditText toolbar_search_bar = findViewById(R.id.toolbar_search_bar);
+        ImageButton SearchButton = findViewById(R.id.toolbar_search_button);
+        ImageButton dropdownButton = findViewById(R.id.toolbar_dropdown_button);
+
+
+        // Inflate the header layout and set it to the navigation view
+        View headerView = LayoutInflater.from(this).inflate(R.layout.header, navigationView, false);
+        navigationView.addHeaderView(headerView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.nav_bus:
+                        Log.i("MENU_DRAWER_TAG", "Bus item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_login:
+                        Log.i("MENU_DRAWER_TAG", "Login item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        // Set a click listener to the dropdown button
+        dropdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(feed_page.this, view);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.dropdown_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        // Handle dropdown menu item clicks here
+                        switch (item.getItemId()) {
+                            case R.id.account_button:
+                                // Handle menu item 1 click
+                                return true;
+                            case R.id.settings_button:
+                                // Handle menu item 2 click
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popup.show();
+            }
+        });
+        /*
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_open, R.string.menu_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.nav_bus:
+                        Log.i("MENU_DRAWER_TAG", "Bus item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_login:
+                        Log.i("MENU_DRAWER_TAG", "Login item is clicked");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+
+                return true;
+            }
+        });
+        */
     }
 }
